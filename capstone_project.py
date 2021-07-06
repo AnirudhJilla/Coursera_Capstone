2d988f3c3289d4f17e7a94b7b14655390d21c53a
AnirudhJilla <62832770+AnirudhJilla@users.noreply.github.com>
2021-07-06 20:49:45 +0530

    Add files via upload

---
+++ capstone_project.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1> Welcome to my Coursera_Capstone project for IBM Data Science Professional certificate</h1>
+# <h2> This notebook will contain all my work related to Coursera_Capstone project</h2>
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+print ('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
